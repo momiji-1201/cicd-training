051d5ee562050b7d2009de23ceced8b3b07453eb
sato momiji <sato.momiji@compass-japan.com>
2025-07-02 17:58:05 +0900

    CICD_SampleTest作成_0702

---
+++ demo/src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void numAssert(){
+        Integer num = 2 + 3;
+        assertEquals(5, num);
+    }
+}
